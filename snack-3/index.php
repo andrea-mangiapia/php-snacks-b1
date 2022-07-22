2107f1234676ab9acc39cc583f6118e80538ce10
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-22 21:47:48 +0200

    done snack 3

--- snack-3/index.php
+++ snack-3/index.php
@@ -9,25 +9,24 @@ tenendo conto che l’array non dovrà contenere lo stesso numero più di una vo
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 3</title>
 </head>
-<body>
-    
+<body>   
 
-<?php
-// Creo una variabile con un array vuoto che conterrà i numeri causali
-$arrayNumbers = [];
+    <?php
+    // Creo una variabile con un array vuoto che conterrà i numeri causali
+    $arrayNumbers = [];
 
-// Creo un ciclo while che ha lo scopo di creare numeri random per una lunghezza massima del'array (in questo caso 15)
-while(count($arrayNumbers) < 15){
-    // Genero un numero random tra 1 e 100
-    $randomNumber = rand(1,100);
-    // pusho il numero random creato nell'array se non è già presente
-    if(!in_array($randomNumber, $arrayNumbers)){
-        $arrayNumbers[] = $randomNumber;
+    // Creo un ciclo while che ha lo scopo di creare numeri random per una lunghezza massima del'array (in questo caso 15)
+    while(count($arrayNumbers) < 15){
+        // Genero un numero random tra 1 e 100
+        $randomNumber = rand(1,100);
+        // pusho il numero random creato nell'array se non è già presente
+        if(!in_array($randomNumber, $arrayNumbers)){
+            $arrayNumbers[] = $randomNumber;
+        }
     }
-}
-// Stampo i valori della variabile
-var_dump($arrayNumbers);
-?>
+    // Stampo i valori della variabile
+    var_dump($arrayNumbers);
+    ?>
 
 </body>
 </html>
