d19404eb8c494c275814f36798ed1b0734a033d7
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-07-22 19:03:51 +0200

    done snack 2

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,5 +1,27 @@
-<?php
+<!-- Passare come parametri GET name, mail e age e verificare 
+(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
+ che mail contenga un punto e una chiocciola e che age sia un numero. 
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
+ <?php
+
+// Passo come parametri GET name, mail e age
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
+// Variabile che conterrà il messaggio per l'utente
+$textAccesMessage;
+
+// SE name è più lungo di 3 caratteri e(&&)
+// SE mail contiene un punto e una chiocciola ed è diverso(!==) da false e(&&)
+// SE mail contiene una chiocciola ed è diverso(!==) da false e(&&)
+// SE age è un numero is_numeric = true (se viene inserito una stringa is_numeric = false)
+if(strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age)) {
+    $textAccesMessage = 'Riuscito';
+} else {
+    $textAccesMessage = 'Negato';
+}
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +30,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack 1</title>
+    <title>Snack 2</title>
 </head>
 <body>
+
+    <!-- Stampo in pagina -->
+    <h2>
+        Accesso <?php echo $textAccesMessage; ?>
+    </h2>
     
 </body>
 </html>
